03edee9ea93c7b713de1945a37252297126f530a
ThaiNguyen <Catraconnguyen76@gmail.com>
2026-03-12 23:34:50 +0700

    PHẦN 1 – LINQ CƠ BẢN (1–7)

---
+++ bai_6/bai_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/* * THONG TIN SINH VIEN:
+ * Ma SV: 2415053122224
+ * Ho va ten: Dang Thai Nguyen
+ * Lop: 225LTC#01
+ */
+
+public class BaiTapLINQ
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ma SV: 2415053122224");
+        Console.WriteLine("Ten SV: Dang Thai Nguyen");
+        Console.WriteLine("Lop  : 225LTC#01");
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+        Console.WriteLine("Danh sach goc: " + string.Join(", ", numbers));
+        int countEven = numbers.Count(n => n % 2 == 0);
+        Console.WriteLine("So luong cac so chan trong danh sach la: " + countEven);
+        Console.ReadLine();
+    }
+}
