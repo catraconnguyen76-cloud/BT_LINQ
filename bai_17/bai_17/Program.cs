e905ac43b4ee7613bdb5f8ce66e00757bec034fb
ThaiNguyen <Catraconnguyen76@gmail.com>
2026-03-13 18:59:52 +0700

    PHẦN 3 – LINQ NÂNG CAO (15–20)

---
+++ bai_17/bai_17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/* * THONG TIN SINH VIEN:
+ * Ma SV: 2415053122224
+ * Ho va ten: Dang Thai Nguyen
+ * Lop: 225LTC#01
+ */
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+
+public class BaiTapLINQ
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ma SV: 2415053122224");
+        Console.WriteLine("Ten SV: Dang Thai Nguyen");
+        Console.WriteLine("Lop  : 225LTC#01");
+        List<Student> students = new List<Student>() {
+            new Student{Id=1, Name="An", Score=8},
+            new Student{Id=2, Name="Binh", Score=6},
+            new Student{Id=3, Name="Chi", Score=9},
+            new Student{Id=4, Name="Dung", Score=7}
+        };
+        double avg = students.Average(s => s.Score);
+        Console.WriteLine("Bai 17: Diem trung binh cua sinh vien: " + avg);
+        Console.ReadLine();
+    }
+}
