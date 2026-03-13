cd275bd218fc478076a69b48618a8e398a36e705
ThaiNguyen <Catraconnguyen76@gmail.com>
2026-03-13 18:51:51 +0700

    PHẦN 3 – LINQ NÂNG CAO (15–20)

---
+++ bai_15/bai_15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/* * THONG TIN SINH VIEN:
+ * Ma SV: 2415053122224
+ * Ho va ten: Dang Thai Nguyen
+ * Lop: 225LTC#01
+ */
+public class BaiTapLINQ
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ma SV: 2415053122224");
+        Console.WriteLine("Ten SV: Dang Thai Nguyen");
+        Console.WriteLine("Lop  : 225LTC#01");
+        List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+        Console.WriteLine("Danh sach goc: " + string.Join(" ", numbers));
+        var uniqueNumbers = numbers.Distinct();
+        Console.WriteLine("Bai 15: Danh sach sau khi loai bo so trung:");
+        Console.WriteLine(string.Join(" ", uniqueNumbers));
+        Console.ReadLine();
+    }
+}
