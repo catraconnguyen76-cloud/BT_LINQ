3da48572bd5216a414641d6e1a4892b8d841bff9
ThaiNguyen <Catraconnguyen76@gmail.com>
2026-03-13 19:08:24 +0700

    PHẦN 3 – LINQ NÂNG CAO (15–20)

---
+++ bai_20/bai_20/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/* * THONG TIN SINH VIEN:
+ * Ma SV: 2415053122224
+ * Ho va ten: Dang Thai Nguyen
+ * Lop: 225LTC#01
+ */
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+
+public class BaiTapLINQ
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ma SV: 2415053122224");
+        Console.WriteLine("Ten SV: Dang Thai Nguyen");
+        Console.WriteLine("Lop  : 225LTC#01");
+        List<Student> students = new List<Student>() {
+            new Student{Id=1, Name="An", Score=8},
+            new Student{Id=2, Name="Binh", Score=6},
+            new Student{Id=3, Name="Chi", Score=9},
+            new Student{Id=4, Name="Dung", Score=7}
+        };
+        var top3 = students.OrderByDescending(s => s.Score).Take(3);
+        Console.WriteLine("Bai 20: Top 3 sinh vien diem cao nhat:");
+        foreach (var s in top3) Console.WriteLine("- {0}: {1} diem", s.Name, s.Score);
+        Console.ReadLine();
+    }
+}
